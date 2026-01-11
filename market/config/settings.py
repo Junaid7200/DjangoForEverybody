835727ebb4013665246eb815e2ec9c85f0319479
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-11 21:53:47 +0000

    chore: cleanup

--- market/config/settings.py
+++ market/config/settings.py
@@ -18,9 +18,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-APP_NAME = "Chuck's Marketplace"   # Change
+APP_NAME = "Junaid's Marketplace"   # Change
 
-DJ4E_CODE = '42'
+DJ4E_CODE = '10181139efb3351f1eff8251990ff12a'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-yb3=cy&k285b#=!h42%bf+g*07v4lrr+l)fj+_tf1e348fpnkj"
