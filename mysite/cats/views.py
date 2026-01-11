a59ab37b646e844f602127507e92f270eaec10ab
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-11 22:21:56 +0500

    feat: code the cat CBVs

--- mysite/cats/views.py
+++ mysite/cats/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from cats.models import Cat, Breed
 
 # Create your views here.
+
+class CatList(LoginRequiredMixin, ListView):
+    model = Cat
+    template_name = "cats/cat_list.html"
+    context_object_name = "cats"
+
+class CatCreate(LoginRequiredMixin, CreateView):
+    model = Cat
+    template_name = "cats/cat_form.html"
+    fields = "__all__"
+    success_url = reverse_lazy("cat_list")
+
+class CatUpdate(LoginRequiredMixin, UpdateView):
+    model = Cat
+    template_name = "cats/cat_form.html"
+    fields = "__all__"
+    success_url = reverse_lazy("cat_list")
+
+class CatDelete(LoginRequiredMixin, DeleteView):
+    model = Cat
+    template_name = "cats/cat_confirm_delete.html"
+    success_url = reverse_lazy("cat_list")
+
