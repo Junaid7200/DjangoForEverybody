da01f99f8e13970ac73f1307a39b17b7faa6f59f
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-23 17:36:00 +0500

    feat: implement get and post methods for create and update ads

--- market/mkt/views.py
+++ market/mkt/views.py
@@ -1,6 +1,11 @@
 from . import models
-
-from mkt.owner import OwnerCreateView, OwnerDeleteView, OwnerDetailView, OwnerListView, OwnerUpdateView
+from mkt.forms import CreateForm
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from django.urls import reverse_lazy
+from mkt.owner import OwnerDeleteView, OwnerDetailView, OwnerListView
 
 # Create your views here.
 class AdListView(OwnerListView):
@@ -12,14 +17,56 @@ class AdDetailView(OwnerDetailView):
     model = models.Ad
     fields = ['title', 'price', 'text']
     template_name = 'mkt/ad_detail.html'
-class AdCreateView(OwnerCreateView):
-    model = models.Ad
-    fields = ['title', 'price', 'text']
+class AdCreateView(LoginRequiredMixin, View):
     template_name = 'mkt/ad_form.html'
-class AdUpdateView(OwnerUpdateView):
-    model = models.Ad
-    fields = ['title', 'price', 'text']
+    success_url = reverse_lazy('mkt:all')
+
+    def get(self, request, pk=None):
+        form = CreateForm()
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
+
+    def post(self, request, pk=None):
+        form = CreateForm(request.POST, request.FILES or None)
+
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name, ctx)
+
+        # Add owner to the model before saving
+        ad = form.save(commit=False)
+        ad.owner = self.request.user
+        ad.save()
+        return redirect(self.success_url)
+class AdUpdateView(LoginRequiredMixin, View):
     template_name = 'mkt/ad_form.html'
+    success_url = reverse_lazy('mkt:all')
+
+    def get(self, request, pk):
+        ad = get_object_or_404(models.Ad, id=pk, owner=self.request.user)
+        form = CreateForm(instance=ad)
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
+
+    def post(self, request, pk):
+        ad = get_object_or_404(models.Ad, id=pk, owner=self.request.user)
+        form = CreateForm(request.POST, request.FILES or None, instance=ad)
+
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name, ctx)
+
+        ad = form.save(commit=False)
+        ad.save()
+        return redirect(self.success_url)
 class AdDeleteView(OwnerDeleteView):
     model = models.Ad
     template_name = 'mkt/ad_confirm_delete.html'
+
+def stream_file(request, pk):
+    pic = get_object_or_404(models.Ad, id=pk)
+    response = HttpResponse()
+    response['Content-Type'] = pic.content_type
+    response['Content-Length'] = len(pic.picture)
+    response.write(pic.picture)
+    return response
