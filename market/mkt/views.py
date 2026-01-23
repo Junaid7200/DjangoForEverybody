ef557ad659f2c9c7bcb2434129fa53d434f8e901
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-23 11:59:47 +0500

    feat: code the CRUD views for ad model inheriting the owner views

--- market/mkt/views.py
+++ market/mkt/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from . import models
+
+from market.mkt.owner import OwnerCreateView, OwnerDeleteView, OwnerDetailView, OwnerListView, OwnerUpdateView
 
 # Create your views here.
+class AdListView(OwnerListView()):
+    model = models.Ad
+    template_name = 'mkt/ad_list.html'
+class AdDetailView(OwnerDetailView()):
+    model = models.Ad
+    template_name = 'mkt/ad_detail.html'
+class AdCreateView(OwnerCreateView()):
+    model = models.Ad
+    fields = ['title', 'price', 'text']
+    template_name = 'mkt/ad_form.html'
+class AdUpdateView(OwnerUpdateView()):
+    model = models.Ad
+    fields = ['title', 'price', 'text']
+    template_name = 'mkt/ad_form.html'
+class AdDeleteView(OwnerDeleteView()):
+    model = models.Ad
+    template_name = 'mkt/ad_confirm_delete.html'
