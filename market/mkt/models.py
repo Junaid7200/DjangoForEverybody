3c51de736f1506e070c8835c55c497155cece6af
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-23 11:40:54 +0500

    feat: code the ad models in mkt app

--- market/mkt/models.py
+++ market/mkt/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.conf import settings
 
 # Create your models here.
+class Ad(models.Model):
+    title = models.CharField(max_length=200, validators=[MinLengthValidator(2, "Title must be at least 2 characters long")])
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
+    text = models.TextField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
