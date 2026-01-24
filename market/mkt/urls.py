0d8bb4e0dec70bc4496354d6cb2011b59ebb4e61
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-24 15:24:16 +0500

    chore: fix the formatting

--- market/mkt/urls.py
+++ market/mkt/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
         views.AdDeleteView.as_view(success_url=reverse_lazy('mkt:all')), name='ad_delete'),
     path('ad_picture/<int:pk>', views.stream_file, name='ad_picture'),
     path('ad/<int:pk>/comment', views.CommentCreateView.as_view(), name='ad_comment_create'),
-path('comment/<int:pk>/delete', views.CommentDeleteView.as_view(success_url=reverse_lazy('mkt:all')), name='ad_comment_delete'),
+    path('comment/<int:pk>/delete', views.CommentDeleteView.as_view(success_url=reverse_lazy('mkt:all')), name='ad_comment_delete'),
 ]
